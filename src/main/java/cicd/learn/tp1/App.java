2f97771e7ad0cf8766b9c9f143f0eacf88435fd9
erwandhe59 <erwandhe59@gmail.com>
2024-04-18 09:11:25 +0200

    Description des changements effectués

--- src/main/java/cicd/learn/tp1/App.java
+++ src/main/java/cicd/learn/tp1/App.java
@@ -1,13 +1,10 @@
 package cicd.learn.tp1;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "CICD rocks!" );
+        System.out.println( "Jenkins" );
     }
 }
